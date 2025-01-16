9850d3df0824574d926632dd4f50f1a250e8d1c5
avinash2025-ai <avinash.singh@ascendion.com>
2025-01-16 13:49:11 +0000

    added updates

--- readcsvfile.py
+++ readcsvfile.py
@@ -3,5 +3,6 @@ import pandas as pd
 file_path = "people_data.csv"
 
 df = pd.read_csv(file_path)
+df = df.rename(columns={"Date of birth" : "DOB"})
 
 print(df.to_string())
---
+++ test1.py
@@ -0,0 +1,7 @@
+
+num = int(input("Enter the number : "))
+for i in range(1,num):
+    print(i)
+
+a = (lambda x, y : x*y)
+print(a(7, 19))
