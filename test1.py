a9dbd3b3f747fdf6734e288faf5b8e9e17d75016
avinash2025-ai <avinash.singh@ascendion.com>
2025-01-20 08:57:21 +0000

    testing done

--- test1.py
+++ test1.py
@@ -1,7 +1,16 @@
 
-num = int(input("Enter the number : "))
-for i in range(1,num):
-    print(i)
+a = [1, 2, 2, 3, 4, 4, 5]
 
-a = (lambda x, y : x*y)
-print(a(7, 19))
+# Create an empty list to store unique values
+res = []
+
+# Iterate through each value in the list 'a'
+for val in a:
+  
+    # Check if the value is not already in 'res'
+    if val not in res:
+      
+        # If not present, append it to 'res'
+        res.append(val)
+
+print(res)
