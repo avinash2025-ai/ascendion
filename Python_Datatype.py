0cfcd7d80b4e38c57d68650d904a1d476446ad91
avinash2025-ai <avinash.singh@ascendion.com>
2025-01-21 13:02:15 +0000

    Python Datatype

---
+++ Python_Datatype.py
@@ -0,0 +1,36 @@
+#List
+# student = ["Avinash","Aviraj","Aviyanka","Priyanka","Akshay","Avinash"]
+# print(student)
+# student.append("Gunu")
+
+# for name in student:
+#     print(name)
+
+#Tuples
+#student = ("Avinash","Aviraj","Aviyanka","Priyanka","Akshay","Avinash")
+#print(student)
+#student.append("Gunu")
+
+# for name in student:
+#     print(name)
+
+
+#Sets
+# student = {"Avinash","Aviraj","Aviyanka","Priyanka","Akshay","Avinash"}
+# print(student)
+#student.append("Gunu")
+
+# for name in student:
+#     print(name)
+
+#Disctionary
+student = {
+  "name": "Avinash",
+  "subject": ["Math","Phy","Chem"],
+  "Marks": [98,89,85]
+}
+
+print(student)
+print(student["subject"][0])
+
+
--- test3_csv.py
+++ test3_csv.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
 spark = SparkSession.builder.getOrCreate()
 
 #read CSV into PySpark DataFrame
@@ -6,3 +8,6 @@ df = spark.read.csv('people_data.csv',header=True)
 
 #view resulting DataFrame
 df.show()
+
+df.createOrReplaceTempView("peoples")
+spark.sql("select email,sex from peoples").show()
