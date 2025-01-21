8e3d5fe5d590ab4ebe1dd492292e0e27f21c1200
avinash2025-ai <avinash.singh@ascendion.com>
2025-01-21 13:19:32 +0000

    remove duplicate from list

--- Python_Datatype.py
+++ Python_Datatype.py
@@ -1,10 +1,10 @@
 #List
-# student = ["Avinash","Aviraj","Aviyanka","Priyanka","Akshay","Avinash"]
-# print(student)
-# student.append("Gunu")
+student = ["Avinash","Aviraj","Aviyanka","Priyanka","Akshay","Avinash"]
+print(list(set(student)))
+#student.append("Gunu")
 
-# for name in student:
-#     print(name)
+for name in list(set(student)):
+    print(name)
 
 #Tuples
 #student = ("Avinash","Aviraj","Aviyanka","Priyanka","Akshay","Avinash")
@@ -24,13 +24,13 @@
 #     print(name)
 
 #Disctionary
-student = {
-  "name": "Avinash",
-  "subject": ["Math","Phy","Chem"],
-  "Marks": [98,89,85]
-}
+# student = {
+#   "name": "Avinash",
+#   "subject": ["Math","Phy","Chem"],
+#   "Marks": [98,89,85]
+# }
 
-print(student)
-print(student["subject"][0])
+# print(student)
+# print(student["subject"][0])
 
 
