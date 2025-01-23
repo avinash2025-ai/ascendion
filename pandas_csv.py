37f25246e9e04be817598bfc325a27cb626da0a9
avinash2025-ai <avinash.singh@ascendion.com>
2025-01-23 04:36:07 +0000

    Pandas Course

--- pandas_csv.py
+++ pandas_csv.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 print(pd.options.display.max_rows)
 df = pd.read_csv("people_data.csv")
-print(df.to_string())
+print(df.head(2))
+print(df.tail(2))
+
+print(df.info())
+
+print(df.duplicated())
---
+++ pandas_json.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#Json file
+
+# df = pd.read_json("sample1.json")
+# print(df.to_string())
+
+df = pd.read_json("sample2.json")
+print(df.to_string())
